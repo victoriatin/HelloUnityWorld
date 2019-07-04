ee82a40f508d5c7a0d557b4e718fbcc92b7b1e64
victoriatin <52183531+victoriatin@users.noreply.github.com>
2019-07-03 22:39:02 -0400

    Added the words Hello from Victoria Tinsley! after user presses space

---
+++ HelloUnityWorld/Assets/ChangeName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ChangeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TMPro.TextMeshProUGUI myText = GetComponent<TextMeshProUGUI>();
+        if(Input.GetKeyDown("space")) {
+            myText.SetText("Hello World from Victoria Tinsley!");
+            Debug.Log("Key Pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+}
